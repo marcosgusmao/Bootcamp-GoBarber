917560cc92bcfe291751eacfda68c45014d4675a
marcosgusmao <marcosgusmao@hotmail.com>
2020-04-22 11:37:37 -0300

    upload de arquivos e tratativas de erro

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -4,6 +4,8 @@ import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
+import AppError from '../errors/AppError';
+
 interface Request {
   provider_id: string;
   date: Date;
@@ -20,7 +22,7 @@ class CreateAppointmentSerice {
     );
 
     if (findAppointmentInSameDate) {
-      throw Error('This Appointment is already booked');
+      throw new AppError('This Appointment is already booked');
     }
 
     const appointment = appointmentsRepository.create({
