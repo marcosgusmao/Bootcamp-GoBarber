95b3ee945c8f4a5a05475f898919e4fca591b63f
marcosgusmao <marcosgusmao@hotmail.com>
2020-06-13 12:49:19 -0300

    regras de agendamento

--- README.md
+++ README.md
@@ -39,7 +39,7 @@
 - O usuário deve poder listar os dias de um mês com pelo menos um horário disponivel  de um
 prestador; ✔︎
 - O usuário deve poder listar horários disponíveis em um dia especifico de um prestador; ✔︎
-- O usuário deve poder realizar um novo agendamento com um prestador;
+- O usuário deve poder realizar um novo agendamento com um prestador; ✔︎
 
 **RNF**
 
@@ -47,8 +47,8 @@ prestador; ✔︎
 
 **RN**
 
-- Cada agendamento deve durar 1h exatamente;
+- Cada agendamento deve durar 1h exatamente; ✔︎
 - Os agendamentos devem estar disponíveis entre 8h às 18h (Primeiro às 8h, último ás 17h);
-- O usuário não pode agendar em um horário já ocupado;
+- O usuário não pode agendar em um horário já ocupado; ✔︎
 - O usuário não pode agendar em um horário que já passou;
 - O usuárionão pode agendar serviços consigo mesmo;
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
@@ -7,18 +7,37 @@ import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
   provider_id: string;
+  user_id: string;
   date: Date;
 }
 
 @injectable()
-class CreateAppointmentSerice {
+class CreateAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
   ) {}
 
-  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
+  public async execute({
+    date,
+    provider_id,
+    user_id,
+  }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
+
+    if (isBefore(appointmentDate, new Date(2020, 4, 10, 12))) {
+      throw new AppError("You Can't create an appointment on a past date.");
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError("You Can't create an appointment with yourself.");
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        'You can only create appointments between 8am and 5pm',
+      );
+    }
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
@@ -30,6 +49,7 @@ class CreateAppointmentSerice {
 
     const appointment = await this.appointmentsRepository.create({
       provider_id,
+      user_id,
       date: appointmentDate,
     });
 
@@ -37,4 +57,4 @@ class CreateAppointmentSerice {
   }
 }
 
-export default CreateAppointmentSerice;
+export default CreateAppointmentService;
