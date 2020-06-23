45b0a0c6dfcf985ad6803ca56bd66c9a59064170
marcosgusmao <marcosgusmao@hotmail.com>
2020-06-23 15:29:44 -0300

    Ajustes na Sessions

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -10,7 +10,6 @@ sessionsRouter.post(
   '/',
   celebrate({
     [Segments.BODY]: {
-      name: Joi.string().required(),
       email: Joi.string().email().required(),
       password: Joi.string().required(),
     },
